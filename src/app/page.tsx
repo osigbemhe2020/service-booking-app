d027ddb7d5136e304d70de6f2f7985f85abaa054
sophiee <xophieadrian@gmail.com>
2025-10-13 17:24:07 +0100

    added primary button

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
+import PrimaryBtn from "@/components/primaryBtn";
 import Textarea from "@/components/textarea";
 
 export default function Home() {
   return (
     <div>
       Welcome to the Landing Page
-      <Textarea text="hello" placeholder="hello" htmlFor="hello" />
+     
     </div>
   );
 }
---
+++ src/components/primaryBtn.tsx
@@ -0,0 +1,24 @@
+type PrimaryBtnProps = {
+  text: string;
+  bgColor: string;
+  textColor: string;
+  hoverColor: string;
+};
+
+const PrimaryBtn = ({
+  text,
+  bgColor,
+  textColor,
+  hoverColor,
+}: PrimaryBtnProps) => {
+  return (
+    <button
+      type="button"
+      className={`${textColor} ${bgColor} hover:${hoverColor} focus:ring-4 focus:${hoverColor} font-medium rounded-lg text-sm px-5 py-2.5`}
+    >
+      {text}
+    </button>
+  );
+};
+
+export default PrimaryBtn;
